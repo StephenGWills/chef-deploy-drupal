6136984e313a2075aa9a0d2be44f110daa70dd45
StephenGWills <swills@beyond-print.com>
2015-01-20 19:59:11 -0500

    Update metadata.rb
    apt is not in the cookbooks at this time.  trying to comment it out as a test.

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs/Configures/Bootsraps Drupal"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "apt"
+#depends "apt"
 depends "build-essential"
 depends "git"
 depends "vim"
